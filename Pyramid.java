f5533a76789d08bad086051bbe1223e1b8f6b8d7
Max201603 <dong.qin@rmit.edu.au>
2016-04-17 17:03:40 +1000

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
-public class Pyramid {
 
-	public static void main(String[] args)  {
+public class Pyramid {
+	public static void main(String[] args) {
 		System.out.print("Please input the layer of the Pyramid:");
-		int layer=new Scanner(System.in).nextInt();
-		for(int i=0;i<layer;i++){
-			printSpace(layer-i-1);
-			printStar(2*i+1);
+		int layer = new Scanner(System.in).nextInt();
+		for (int i = 0; i < layer; i++) {
+			printSpace(layer - i - 1);
+			printStar(2 * i + 1);
 			System.out.println();
 		}
 	}
-	static void printSpace(int i){
-		for(int j=0;j<i;j++){
+
+	static void printSpace(int i) {
+		for (int j = 0; j < i; j++) {
 			System.out.print(" ");
 		}
 	}
-	static void printStar(int i){
-		for(int j=0;j<i;j++){
+
+	static void printStar(int i) {
+		for (int j = 0; j < i; j++) {
 			System.out.print("*");
 		}
 	}
-	
-	
-	
-
 }
